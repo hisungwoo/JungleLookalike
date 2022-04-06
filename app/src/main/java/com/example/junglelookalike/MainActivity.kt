dc3438bbe4912eb84e710f4c390c39d20962fda2
hisun <hisungwoo007@naver.com>
2022-04-06 23:37:18 +0900

    1.mainViewModel 추가 2.viewBinding

--- app/src/main/java/com/example/junglelookalike/MainActivity.kt
+++ app/src/main/java/com/example/junglelookalike/MainActivity.kt
@@ -2,10 +2,18 @@ package com.example.junglelookalike
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.activity.viewModels
+import com.example.junglelookalike.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    val mainViewModel : MainViewModel by viewModels()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        val binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+
+
     }
 }
---
+++ app/src/main/java/com/example/junglelookalike/MainViewModel.kt
@@ -0,0 +1,9 @@
+package com.example.junglelookalike
+
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
+
+class MainViewModel(application : Application) : AndroidViewModel(application) {
+
+
+}
