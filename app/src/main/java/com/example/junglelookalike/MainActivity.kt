24ff2550b4f4c2956c39ae24a99773b260e12b74
hisun <hisungwoo007@naver.com>
2022-04-14 23:35:11 +0900

    1. teachable machine , tflite 추가

--- app/src/main/java/com/example/junglelookalike/MainActivity.kt
+++ app/src/main/java/com/example/junglelookalike/MainActivity.kt
@@ -4,6 +4,9 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.activity.viewModels
 import com.example.junglelookalike.databinding.ActivityMainBinding
+import com.example.junglelookalike.ml.Model
+import org.tensorflow.lite.DataType
+import org.tensorflow.lite.support.tensorbuffer.TensorBuffer
 
 class MainActivity : AppCompatActivity() {
     val mainViewModel : MainViewModel by viewModels()
@@ -13,7 +16,21 @@ class MainActivity : AppCompatActivity() {
         val binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        val model = Model.newInstance(this)
+
+        // Creates inputs for reference.
+        val inputFeature0 = TensorBuffer.createFixedSize(intArrayOf(1, 224, 224, 3), DataType.FLOAT32)
+//        inputFeature0.loadBuffer(byteBuffer)
+
+        // Runs model inference and gets result.
+        val outputs = model.process(inputFeature0)
+        val outputFeature0 = outputs.outputFeature0AsTensorBuffer
+
+        // Releases model resources if no longer used.
+        model.close()
+
 
 
     }
+
 }
